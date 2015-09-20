569ce744ab79a562e7d5a43e6023edad73f31b10
Joshua Chaitin-Pollak <josh@offthehill.org>
2015-09-20 13:15:30 -0400

    Naive implementation

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
 
+
 int main(int argc, char ** argv)
 {
-	std::cout << "Pepito\n";
+	/*
+	 * Image Definition
+	 */
+	const unsigned short image[] = {
+			0,0,0,0,0,0,0,0,0,0,
+			0,0,1,1,1,0,0,0,0,0,
+			0,0,1,1,1,1,1,0,0,0,
+			0,0,1,0,0,0,1,0,0,0,
+			0,0,1,1,1,1,1,0,0,0,
+			0,0,0,0,1,0,1,0,0,0,
+			0,0,0,0,1,0,1,0,0,0,
+			0,0,0,0,1,1,1,0,0,0,
+			0,0,0,0,0,0,0,0,0,0,
+			0,0,0,0,0,0,0,0,0,0
+	};
+
+	const unsigned short width = 10;
+	const unsigned short height = 10;
+
+	unsigned short cell_reads = 0;
+
+	unsigned short top = 10;
+	unsigned short left = 10;
+	unsigned short bottom = 0;
+	unsigned short right = 0;
+
+	// Iterate over image height
+	for (unsigned short j = 0; j < height; j++) {
+		// Iterate over image width
+		for (unsigned short i = 0; i < width; i++) {
+			cell_reads++;
+
+			if (image[j*height+i] == 0) {
+				continue;
+			}
+
+			top = (top < j) ? top : j;
+			left = (left < i) ? left : i;
+			bottom = (bottom > j) ? bottom : j;
+			right = (right > i) ? right : i;
+
+
+
+		}
+	}
+
+	std::cout << "Cell Reads: " << cell_reads << "\n";
+	std::cout << "Top: " << top << "\n";
+	std::cout << "Left: " << left << "\n";
+	std::cout << "Bottom: " << bottom << "\n";
+	std::cout << "Right: " << right << "\n";
+
 	return 0;
 }
